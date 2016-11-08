78114acf2f44e9447cd02051f08ae6c8f59beeb3
Wang Ran <1260846272@qq.com>
2016-11-08 16:23:58 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+#author = ‘wangran0721’
 #输入500000个“啪”
 i = 0
 for i in range(500000):
@@ -6,3 +7,17 @@ for i in range(500000):
     i = i + 1
     file.close()
     
+#随机输入500000个汉字
+# encoding=utf8  
+import sys  
+  
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+from random import randint as rint
+
+i = 0 
+file = open('path/test.txt','w+')
+for i in range(500000):
+	print>>file,unichr(rint(19968,40869)),
+	i = i + 1
